907157682040591d93618d6f227ee006620da669
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2017-11-25 14:17:13 -0500

    fix

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const _ = require('underscore');
 
 const api = `https://api.cryptokitties.co/auctions`;
 
-function getKitties(params) {
+function getAuctions(params) {
   return fetch(`${api}?${qs.stringify(params)}`)
     .then(response => response.json());
 }
 
-async function getAllKitties({ type = 'sale', status = 'open' }) {
-  const { total } = await getKitties({ limit: 10000, offset: 0, type, status });
+async function getAllAuctions({ type = 'sale', status = 'open' }) {
+  const { total } = await getAuctions({ limit: 0, offset: 0, type, status });
 
+  // fetch all the expected pages of kitties
   const promises = _.map(
     _.range(0, total, 1000),
-    offset => getKitties({ limit: 1000, offset, type, status })
+    offset => getAuctions({ limit: 1000, offset, type, status })
       .then(({ auctions }) => auctions)
   );
 
-  // split it into chunks
+  // return all the page results
   return _.flatten(await Promise.all(promises));
 }
 
@@ -28,7 +29,8 @@ function print(obj) {
   console.log(JSON.stringify(obj));
 }
 
-getAllKitties({ type: 'sale' })
+// get all sales that are below 0.003 eth
+getAllAuctions({ type: 'sale' })
   .then(auctions => auctions.filter(auction => fromWei(auction.current_price) < 0.003))
   .then(print)
   .catch(error => console.error(error));
