a29ae35c1add263b79c6008fe33cdcd8720cf06a
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2017-11-29 19:43:57 -0500

    just add up the rarities

--- src/util/kittyRarity.js
+++ src/util/kittyRarity.js
@@ -4,5 +4,8 @@ import rarities from '../json/rarities';
 const sum = arr => _.reduce(arr, (memo, num) => memo + num, 0);
 
 export default function kittyRarity({ cattributes }) {
-  return sum(cattributes.map(cattribute => Math.log10(rarities[ cattribute ] || 1))) / cattributes.length;
+  if (cattributes.length === 0) {
+    return Infinity;
+  }
+  return sum(cattributes.map(cattribute => Math.log10(rarities[ cattribute ] || 1)));
 }
